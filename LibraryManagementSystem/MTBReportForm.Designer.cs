ce6267af7dcfbc5ddcb13a60a678623d236b2cf7
phucnguyencld3 <phucnguyencld2006@gmail.com>
2025-07-28 09:47:32 +0700

    Phần giao diện form MTPReportForm và phần code xử lý cho form MTPReportForm

---
+++ LibraryManagementSystem/MTBReportForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GUI_UI
+{
+    partial class MTBReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cboReportType = new ComboBox();
+            btnConfirm = new Button();
+            dgvReport = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvReport).BeginInit();
+            SuspendLayout();
+            // 
+            // cboReportType
+            // 
+            cboReportType.FormattingEnabled = true;
+            cboReportType.Location = new Point(668, 123);
+            cboReportType.Name = "cboReportType";
+            cboReportType.Size = new Size(184, 28);
+            cboReportType.TabIndex = 0;
+            cboReportType.SelectedIndexChanged += cboReportType_SelectedIndexChanged;
+            // 
+            // btnConfirm
+            // 
+            btnConfirm.Location = new Point(869, 123);
+            btnConfirm.Name = "btnConfirm";
+            btnConfirm.Size = new Size(127, 29);
+            btnConfirm.TabIndex = 1;
+            btnConfirm.Text = "Xác nhận";
+            btnConfirm.UseVisualStyleBackColor = true;
+            btnConfirm.Click += btnConfirm_Click;
+            // 
+            // dgvReport
+            // 
+            dgvReport.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvReport.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvReport.Location = new Point(12, 157);
+            dgvReport.Name = "dgvReport";
+            dgvReport.RowHeadersWidth = 51;
+            dgvReport.Size = new Size(1002, 278);
+            dgvReport.TabIndex = 2;
+            // 
+            // MTBReportForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1026, 447);
+            Controls.Add(dgvReport);
+            Controls.Add(btnConfirm);
+            Controls.Add(cboReportType);
+            Name = "MTBReportForm";
+            Text = "MTBReportForm";
+            Load += baform_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvReport).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox cboReportType;
+        private Button btnConfirm;
+        private DataGridView dgvReport;
+    }
+}
---
+++ LibraryManagementSystem/MTBReportForm.cs
@@ -0,0 +1,66 @@
+﻿using BLL_Services.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_UI
+{
+    public partial class MTBReportForm : Form
+    {
+        public MTBReportForm()
+        {
+            InitializeComponent();
+            loadComboBox();
+        }
+        private void loadComboBox()
+        {
+            // Add items to the combo box
+            cboReportType.Items.Add("Sách mượn nhiều nhất");
+            cboReportType.Items.Add("Người mượn hàng đầu");
+            cboReportType.Items.Add("Tình trạng tồn kho sách");
+            // Set the default selected item
+            if (cboReportType.Items.Count > 0)
+            {
+                cboReportType.SelectedIndex = 0;
+            }
+        }
+        private void cboReportType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void baform_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            // Get the selected report type from the combo box
+            string selectedReportType = cboReportType.SelectedItem.ToString();
+            // Create an instance of the ReportService
+            BookService bookService = new BookService();
+            // Call the appropriate method based on the selected report type
+            switch (selectedReportType)
+            {
+                case "Sách mượn nhiều nhất":
+                    dgvReport.DataSource =  bookService.GetTopBorrowedBooks();
+                    break;
+                case "Người mượn hàng đầu":
+                    dgvReport.DataSource = bookService.GetTopReaders();
+                    break;
+                case "Tình trạng tồn kho sách":
+                    dgvReport.DataSource = bookService.GetBookInventoryStatus();
+                    break;
+                default:
+                    MessageBox.Show("Vui lòng chọn loại báo cáo hợp lệ.");
+                    break;
+            }
+        }
+    }
+}
